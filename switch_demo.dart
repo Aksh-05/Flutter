f47ea0f834c7729e23a3f5d7abfeeb8e539acf06
Akshara <163653898+Aksh-05@users.noreply.github.com>
2025-04-22 19:23:36 +0530

    Create switch_demo.dart

---
+++ switch_demo.dart
@@ -0,0 +1,12 @@
+void switchd1(String a) {
+ switch(a)
+ {
+  case 'red': 
+ 
+  case 'blue': print('the colour is blue');
+ 
+  case 'green': print('the colour is green');
+  
+  case _:print('colour is not listed');
+ }
+}
